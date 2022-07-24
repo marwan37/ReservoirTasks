758a7eb702c9b192f602a1c52ebc31779fcff6f6
Marwan Zaarab <marwan.zaarab@gmail.com>
2022-07-24 07:32:01 -0500

    rebundle

--- todo.rb
+++ todo.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require "sinatra/content_for"
 require "tilt/erubis"
+require "pry"
 
 require_relative "database_persistence"
 
@@ -22,14 +23,6 @@ helpers do
 
   def list_class(list)
     "complete" if list_complete?(list)
-  end
-
-  def todos_count(list)
-    list[:todos].size
-  end
-
-  def todos_remaining_count(list)
-    list[:todos].count { |todo| !todo[:completed] }
   end
 
   def sort_lists(lists, &block)
@@ -113,6 +106,7 @@ end
 get "/lists/:id" do
   @list_id = params[:id].to_i
   @list = load_list(@list_id)
+  @todos = @storage.find_todos_for_list(@list_id)
   erb :list, layout: :layout
 end
 
